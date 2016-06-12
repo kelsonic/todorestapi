8104f8264095e91f358bbaf78f5d503e6f14fc49
kelsonic <kgadams93@gmail.com>
2016-06-12 11:50:41 -0700

    Add get routes for /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+  id: 1,
+  description: 'Brush teeth',
+  completed: false
+}, {
+  id: 2,
+  description: 'Go to the grocery store',
+  completed: false
+}, {
+  id: 3,
+  description: 'Buy birthday cake',
+  completed: true
+}];
 
 app.get('/', function(req, res) {
   res.send('Todo API root');
 });
+
+// GET /todos
+app.get('/todos', function(req, res) {
+  res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo;
+
+  todos.forEach(function(todo) {
+    if (todoId === todo.id) {
+      matchedTodo = todo;
+    }
+  })
+
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  } else {
+    res.status(404).send();  
+  }
+  
+})
 
 app.listen(PORT, function() {
   console.log("Express listening on port " + PORT + "!");
