333f9005c5261c6bfd7ea445ee63e3272ee66b03
kelsonic <kgadams93@gmail.com>
2016-06-12 22:06:44 -0700

    Install underscore, refactor /todos/:id route, and add validations for description and completed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,14 +21,9 @@ app.get('/todos', function(req, res) {
 
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
+  
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
-
-  todos.forEach(function(todo) {
-    if (todoId === todo.id) {
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
   if (matchedTodo) {
     res.json(matchedTodo);
@@ -39,6 +35,14 @@ app.get('/todos/:id', function(req, res) {
 // POST /todos
 app.post('/todos', function(req, res) {
   var body = req.body;
+
+  if (!_.isBoolean(body.completed)) {
+    return res.status(400).send("Completed is not a boolean.");
+  } else if ( !_.isString(body.description) ) {
+    return res.status(400).send("Description is not a string.");
+  } else if ( body.description.trim().length === 0 ) {
+    return res.status(400).send("Description must be present.")
+  }
 
   body.id = todoNextId++;
   todos.push(body);
