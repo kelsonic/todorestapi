4fb0afe9729a4081569de2d698c6fe590f763637
kelsonic <kgadams93@gmail.com>
2016-06-14 14:23:02 -0700

    Add POST /login route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcryptjs');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -128,6 +129,31 @@ app.post('/users', function(req, res) {
     res.status(400).json(e);
   });
 })
+
+
+// POST /login
+app.post('/login', function(req, res) {
+  var body = _.pick(req.body, 'email', 'password');
+
+  if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+    return res.status(400).send();
+  }
+
+  db.user.findOne({
+    where: {
+      email: body.email
+    }
+  }).then(function(user){
+    if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+      return res.status(401).send();
+    }
+
+    res.json(user.toPublicJSON());
+  }, function(e) {
+    res.status(500).send();
+  });
+
+});
 
 
 db.sequelize.sync().then(function() {
