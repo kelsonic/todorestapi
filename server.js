1c049d78e95b8c25f418d63ce05cb18e6ce5374e
kelsonic <kgadams93@gmail.com>
2016-06-14 17:21:06 -0700

    Add DELETE /api/logout

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res) {
 });
 
 // GET /todos?completed=true
-app.get('/todos', middleware.requireAuthentication, function(req, res) {
+app.get('/api/todos', middleware.requireAuthentication, function(req, res) {
   var query = req.query;
   var where = {
     userId: req.user.get('id')
@@ -44,7 +44,7 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
+app.get('/api/todos/:id', middleware.requireAuthentication, function(req, res) {
   
   var todoId = parseInt(req.params.id, 10);
 
@@ -65,7 +65,7 @@ app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', middleware.requireAuthentication, function(req, res) {
+app.post('/api/todos', middleware.requireAuthentication, function(req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
   db.todo.create(body).then(function(todo) {
@@ -81,7 +81,7 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 
 
 // DELETE /todos/:id
-app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
+app.delete('/api/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
 
   db.todo.destroy({
@@ -103,7 +103,7 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 
 // PUT /todos/:id
-app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
+app.put('/api/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
   var body = _.pick(req.body, 'description', 'completed');
   var attributes = {};
@@ -138,7 +138,7 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 
 // POST /users
-app.post('/users', function(req, res) {
+app.post('/api/users', function(req, res) {
   var body = _.pick(req.body, 'email', 'password');
 
   db.user.create(body).then(function(user) {
@@ -150,22 +150,33 @@ app.post('/users', function(req, res) {
 
 
 // POST /login
-app.post('/login', function(req, res) {
+app.post('/api/login', function(req, res) {
   var body = _.pick(req.body, 'email', 'password');
+  var userInstance;
 
   db.user.authenticate(body).then(function(user) {
-    var token = user.generateToken('authentication')
+    var token = user.generateToken('authentication');
+    userInstance = user;
 
-    if (token) {
-      res.header('Auth', token).json(user.toPublicJSON());
-    } else {
-      res.status(401).send();
-    }
+    return db.token.create({
+      token: token
+    });
 
-  }, function() {
+  }).then(function(tokenInstance) {
+    res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+  }).catch(function() {
     res.status(401).send();
   });
+});
 
+
+// DELETE /logout
+app.delete('/api/logout', middleware.requireAuthentication, function(req, res) {
+  req.token.destroy().then(function() {
+    res.status(204).send();
+  }).catch(function() {
+    res.status(500).send();
+  })
 });
 
 
